0f46150c22e268c8f9fbd23431d84cefb3ac27d6
song <nadana0929@gmail.com>
2024-09-12 00:17:37 +0900

    [Add] 프로젝트 생성

---
+++ ScrollPaper/ScrollPaperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollPaperApp.swift
+//  ScrollPaper
+//
+//  Created by Song Kim on 9/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrollPaperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
