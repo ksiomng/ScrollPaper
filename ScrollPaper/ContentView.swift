74cbd391f75f4a12959df6297ef6f7ab0316bd86
song <nadana0929@gmail.com>
2024-09-18 19:25:20 +0900

    [Feat] 스크롤 뷰 옆으로 넘어가게

--- ScrollPaper/ContentView.swift
+++ ScrollPaper/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 import Kingfisher
 
 struct ContentView: View {
-    @State var arr: [String] = ["dsjfl", "djaflsdjfl"]
+    @State var arr: [String] = ["dsjfl", "djaflsdjfl", "spacing: 10"]
     
     init() {
         UITextView.appearance().backgroundColor = .clear
@@ -18,22 +18,26 @@ struct ContentView: View {
     var body: some View {
         
         VStack {
-            ForEach(0..<arr.count, id: \.self) { i in
-                ZStack {
-                    KFImage(URL (string: "https://lh4.googleusercontent.com/proxy/c-v0e5l7AIsDvGqELDtp3j1sWJgchFrVcJrG3DJrkFrieE-OECuOJob3CLdptPp6HfnSrUH3B9WIKQDPMjw1FV1rtVpt-x97EX7cHDcuvlCBqR1NuMS7qTYwbeN1ysuyJ74Dwgi4lvu3SsxSZc5Onw"))
-                        .resizable()
-                        .frame(width: .infinity, height: .infinity)
-                        .clipShape(RoundedRectangle(cornerRadius: 10))
-                    TextEditor(text: $arr[i])
-                        .scrollContentBackground(.hidden)
-                        .background(Color.clear)
-                        .padding(20)
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: UIScreen.main.bounds.width * 0.02) {
+                    ForEach(0..<arr.count, id: \.self) { i in
+                        ZStack {
+                            KFImage(URL (string: "https://lh4.googleusercontent.com/proxy/c-v0e5l7AIsDvGqELDtp3j1sWJgchFrVcJrG3DJrkFrieE-OECuOJob3CLdptPp6HfnSrUH3B9WIKQDPMjw1FV1rtVpt-x97EX7cHDcuvlCBqR1NuMS7qTYwbeN1ysuyJ74Dwgi4lvu3SsxSZc5Onw"))
+                                .resizable()
+                                .clipShape(RoundedRectangle(cornerRadius: 10))
+                            TextEditor(text: $arr[i])
+                                .scrollContentBackground(.hidden)
+                                .background(Color.clear)
+                                .padding(20)
+                        }
+                        .frame(width: 355, height: 450)
+                    }
                 }
-                .frame(height: 200)
-                .padding(.bottom,  50)
+                .padding(.horizontal, UIScreen.main.bounds.width * 0.04)
             }
+            .scrollTargetLayout()
+            .scrollTargetBehavior(.viewAligned)
         }
-        .padding()
     }
 }
 
