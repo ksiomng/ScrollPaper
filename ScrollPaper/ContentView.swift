fe9d566947f890ba3678c2e783457ebda0565614
song <nadana0929@gmail.com>
2024-09-24 15:19:55 +0900

    Fix: 버튼으로 한페이지한페이지 추가되게

--- ScrollPaper/ContentView.swift
+++ ScrollPaper/ContentView.swift
@@ -10,39 +10,42 @@ import Kingfisher
 
 struct ContentView: View {
     @State var textArr: [String] = [""]
-    @State var heightArr: [CGFloat] = []
-    @State var widthArr: [CGFloat] = []
-    
-    @State private var text = ""
-    @State private var height: CGFloat = 50
-    @State private var width: CGFloat = 300
+    @State var heightArr: CGFloat = 0
+    @State var widthArr: CGFloat = 0
+    @State var isButton: Bool = false
     
     init() {
         UITextView.appearance().backgroundColor = .clear
     }
     
     var body: some View {
-        
         VStack {
-            Text("Height: \(height)")
-            Text("Width: \(width)")
-            
             ScrollView(.horizontal, showsIndicators: false) {
                 HStack(spacing: UIScreen.main.bounds.width * 0.04) {
                     ForEach(0..<textArr.count, id: \.self) { i in
                         ZStack {
-                            KFImage(URL (string: "https://lh4.googleusercontent.com/proxy/c-v0e5l7AIsDvGqELDtp3j1sWJgchFrVcJrG3DJrkFrieE-OECuOJob3CLdptPp6HfnSrUH3B9WIKQDPMjw1FV1rtVpt-x97EX7cHDcuvlCBqR1NuMS7qTYwbeN1ysuyJ74Dwgi4lvu3SsxSZc5Onw"))
+                            KFImage(URL(string: "https://lh4.googleusercontent.com/proxy/c-v0e5l7AIsDvGqELDtp3j1sWJgchFrVcJrG3DJrkFrieE-OECuOJob3CLdptPp6HfnSrUH3B9WIKQDPMjw1FV1rtVpt-x97EX7cHDcuvlCBqR1NuMS7qTYwbeN1ysuyJ74Dwgi4lvu3SsxSZc5Onw"))
                                 .resizable()
-                            VStack {
-                                GeometryReader { geo in
-                                    CustomTextEditor(text: $text, height: $height, width: $width, maxHeight: geo.size.height, maxWidth: geo.size.width)
-                                        .frame(width: width, height: height)
+                            
+                            GeometryReader { geo in
+                                VStack {
+                                    CustomTextEditor(text: $textArr[i], height: $heightArr, width: $widthArr, maxHeight: geo.size.height, maxWidth: geo.size.width)
+                                        .frame(width: geo.size.width, height: geo.size.height)
                                         .border(Color.gray, width: 1)
+                                        .onChange(of: heightArr) {
+                                            if heightArr >= geo.size.height - 20 && i == textArr.count - 1 {
+                                                isButton = true
+                                            } else {
+                                                isButton = false
+                                            }
+                                        }
                                 }
-                                
-                                Spacer()
                             }
                             .padding(UIScreen.main.bounds.width * 0.1)
+                            
+                            if isButton {
+                                Button{"+"}
+                            }
                         }
                         .aspectRatio(9/13, contentMode: .fit)
                         .frame(width: UIScreen.main.bounds.width * 0.88)
@@ -52,14 +55,6 @@ struct ContentView: View {
             }
             .scrollTargetLayout()
             .scrollTargetBehavior(.viewAligned)
-        }
-    }
-    
-    func setTextArr() {
-        for i in 0..<textArr.count {
-            if heightArr[i] > 50 {
-                
-            }
         }
     }
 }
@@ -74,29 +69,30 @@ struct CustomTextEditor: UIViewRepresentable {
     @Binding var width: CGFloat
     var maxHeight: CGFloat
     var maxWidth: CGFloat
-
+    
     class Coordinator: NSObject, UITextViewDelegate {
         var parent: CustomTextEditor
-
+        
         init(_ parent: CustomTextEditor) {
             self.parent = parent
         }
-
+        
         func textViewDidChange(_ textView: UITextView) {
             parent.text = textView.text
-
+            
             let size = textView.sizeThatFits(CGSize(width: parent.maxWidth, height: CGFloat.greatestFiniteMagnitude))
+            
             DispatchQueue.main.async {
-                self.parent.height = min(size.height, self.parent.maxHeight)
-                self.parent.width = min(size.width, self.parent.maxWidth)
+                self.parent.height = size.height
+                self.parent.width = size.width
             }
         }
     }
-
+    
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
     }
-
+    
     func makeUIView(context: Context) -> UITextView {
         let textView = UITextView()
         textView.delegate = context.coordinator
@@ -108,20 +104,22 @@ struct CustomTextEditor: UIViewRepresentable {
         textView.font = UIFont.systemFont(ofSize: 15)
         
         let maxWidthConstraint = NSLayoutConstraint(item: textView,
-                                                    attribute: .width,
-                                                    relatedBy: .lessThanOrEqual,
-                                                    toItem: nil,
-                                                    attribute: .notAnAttribute,
-                                                    multiplier: 1.0,
-                                                    constant: maxWidth)
-        
+                                                     attribute: .width,
+                                                     relatedBy: .lessThanOrEqual,
+                                                     toItem: nil,
+                                                     attribute: .notAnAttribute,
+                                                     multiplier: 1.0,
+                                                     constant: maxWidth)
+         
         textView.addConstraint(maxWidthConstraint)
         
         return textView
     }
-
+    
     func updateUIView(_ uiView: UITextView, context: Context) {
+        if uiView.text == text {
+            return
+        }
         uiView.text = text
     }
 }
-
